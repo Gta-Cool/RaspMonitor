eefc0b4ca92b4fdc735ddada12adac09c496ce34
Jonathan Plugaru <jonathanp@iron-mail.net>
2015-03-27 18:43:21 +0000

    [fix/api-bundle] Fixing the name and namespace for the DreamedAtlasApiBundle.

---
+++ src/DreamedAtlas/Bundle/ApiBundle/Controller/ApiController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DreamedAtlas\Bundle\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ApiController extends Controller
+{
+}
---
+++ src/DreamedAtlas/Bundle/ApiBundle/DreamedAtlasApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DreamedAtlas\Bundle\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DreamedAtlasApiBundle extends Bundle
+{
+}
